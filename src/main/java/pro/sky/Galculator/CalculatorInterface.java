320bd32b6bb140ca9f70e0e1a7de7c2a4c9fde3f
REIGaruki <gyakujo@mail.ru>
2024-04-29 10:29:11 +0300

    Methods

--- src/main/java/pro/sky/Galculator/CalculatorInterface.java
+++ src/main/java/pro/sky/Galculator/CalculatorInterface.java
@@ -1,4 +1,10 @@
 package pro.sky.Galculator;
 
 public interface CalculatorInterface {
+    String greet();
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiplicate(double a, double b);
+    double divide(double a, double b);
+
 }
--- src/main/java/pro/sky/Galculator/CalculatorService.java
+++ src/main/java/pro/sky/Galculator/CalculatorService.java
@@ -1,4 +1,28 @@
 package pro.sky.Galculator;
 
-public class CalculatorService {
+public class CalculatorService implements CalculatorInterface{
+    @Override
+    public String greet() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @Override
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double multiplicate(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public double divide(double a, double b) {
+        return a / b;
+    }
 }
