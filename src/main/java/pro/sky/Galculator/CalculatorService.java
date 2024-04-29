b6dd44a028d5731e800c22e00ce3bb141e7ad365
REIGaruki <gyakujo@mail.ru>
2024-04-29 12:20:15 +0300

    Errors

--- src/main/java/pro/sky/Galculator/CalculatorService.java
+++ src/main/java/pro/sky/Galculator/CalculatorService.java
@@ -1,8 +1,6 @@
 package pro.sky.Galculator;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class CalculatorService implements CalculatorInterface{
