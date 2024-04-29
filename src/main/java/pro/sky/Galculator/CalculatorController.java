a23abae1f6b617def2ebfa34406a6bed1f6b3483
REIGaruki <gyakujo@mail.ru>
2024-04-29 11:52:12 +0300

    Ьфззштп

--- src/main/java/pro/sky/Galculator/CalculatorController.java
+++ src/main/java/pro/sky/Galculator/CalculatorController.java
@@ -1,40 +1,42 @@
 package pro.sky.Galculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/calculator")
 public class CalculatorController {
     private final CalculatorInterface calculatorService;
 
     public CalculatorController(CalculatorInterface calculatorService) {
         this.calculatorService = calculatorService;
     }
-    @GetMapping(path="/calculator")
+    @GetMapping
     public String greet() {
         return calculatorService.greet();
     }
-    @GetMapping(path="/calculator/plus")
+    @GetMapping(path="/plus")
     public String add(@RequestParam(value = "num1", required = true) double a,
                       @RequestParam(value = "num2", required = true) double b) {
         return a + " + " + b + " = " + calculatorService.add(a, b);
     }
-    @GetMapping(path="/calculator/minus")
+    @GetMapping(path="/minus")
     public String subtract(@RequestParam(value = "num1", required = true) double a,
                            @RequestParam(value = "num2", required = true) double b) {
         return a + " - " + b + " = " + calculatorService.subtract(a, b);
     }
-    @GetMapping(path="/calculator/multiply")
+    @GetMapping(path="/multiply")
     public String multiply(@RequestParam(value = "num1", required = true) double a,
                            @RequestParam(value = "num2", required = true) double b) {
         return a + " * " + b + " = " + calculatorService.multiply(a, b);
     }
-    @GetMapping(path="/calculator/divide")
+    @GetMapping(path="/divide")
     public String divide(@RequestParam(value = "num1", required = true) double a,
                          @RequestParam(value = "num2", required = true) double b) {
         if (b == 0) {
-            return "Division by zero Error";
+            return "Деление на ноль недопустимо";
         } else {
             return a + " / " + b + " = " + calculatorService.divide(a, b);
         }
